5fada08a60f699858a42dce0e6a21ea20ce17253
Tibo Mogul <tibo.mogul@gmail.com>
2015-11-12 20:35:37 +0800

    fake vms actions for dev testing

--- app/controllers/vms_controller.rb
+++ app/controllers/vms_controller.rb
@@ -1,11 +1,15 @@
 class VmsController < ApplicationController
-  soap_service namespace: 'urn:WashOut', wsdl_style: 'document'
-  
+  soap_service namespace: 'urn:vms', wsdl_style: 'document'
+
+  DEFAULT_OK_RESPONSE =  %Q{<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+      <soapenv:Body>THIS IS THE FAKE WEB SERVICE REPLYING OK</soapenv:Body>
+    </soapenv:Envelope>}
+
   soap_action 'getAvailability',
     args: [:string],
     return: :string
   def getAvailability
-    render soap: %Q{<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+    render xml: %Q{<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
       <soapenv:Body>
         <vms:availabilityResponse xmlns:vms="http://www.sandstone-vms.com.au/schema/vms/1.0">
           <vms:status code="AVAILABLE" />
@@ -14,4 +18,19 @@ class VmsController < ApplicationController
       </soapenv:Body>
     </soapenv:Envelope>} and return
   end
+
+  soap_action 'acceptValuationRequest',
+    args: [:string],
+    return: :string
+  def acceptValuationRequest
+    render xml: DEFAULT_OK_RESPONSE and return
+  end
+
+  soap_action 'assignValuationRequest',
+    args: [:string],
+    return: :string
+  def assignValuationRequest
+    render xml: DEFAULT_OK_RESPONSE and return
+  end
+
 end
