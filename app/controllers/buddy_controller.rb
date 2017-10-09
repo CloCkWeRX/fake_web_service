33411298ebd339b8046203a0fd576ec0e80132f1
Daniel O'Connor <daniel.oconnor@htw.com.au>
2017-10-09 14:58:17 +1030

    HAWK-6175 Simulate slow responses

--- app/controllers/buddy_controller.rb
+++ app/controllers/buddy_controller.rb
@@ -1,5 +1,6 @@
 class BuddyController < ApplicationController
   def make_call
+    sleep(120) if params[:destination] == '0427698700' # see https://herrontoddwhite.atlassian.net/browse/HAWK-6122
     render nothing: true
   end
 end
